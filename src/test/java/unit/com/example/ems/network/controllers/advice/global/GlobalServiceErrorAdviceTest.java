0bf19b34581c591c0113e54024966af51dd0391a
Ihor Hryshkov <igor.grishkov.olegovich@gmail.com>
2020-08-23 08:13:25 +0300

    PVC-148 - Add mocks injections for error advice tests.

--- src/test/java/unit/com/example/ems/network/controllers/advice/global/GlobalServiceErrorAdviceTest.java
+++ src/test/java/unit/com/example/ems/network/controllers/advice/global/GlobalServiceErrorAdviceTest.java
@@ -1,14 +1,36 @@
 package unit.com.example.ems.network.controllers.advice.global;
 
+import com.example.ems.config.messages.Messages;
+import com.example.ems.dto.network.controller.Message;
+import com.example.ems.network.controllers.advice.global.GlobalServiceErrorAdvice;
+import com.example.ems.utils.network.Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ExtendWith(MockitoExtension.class)
 class GlobalServiceErrorAdviceTest {
+  @Mock
+  private Response<Object>   response;
+  @Mock
+  private Messages           messages;
+  @Mock
+  private Message            message;
+  @Mock
+  private HttpServletRequest req;
+  @Mock
+  private Exception ex;
+
+  @InjectMocks
+  private GlobalServiceErrorAdvice globalServiceErrorAdvice;
 
   @Test
-  void handleAnyException() {}
+  void handleAnyException() {
+  }
 
   @Test
   void handleValidationException() {}
