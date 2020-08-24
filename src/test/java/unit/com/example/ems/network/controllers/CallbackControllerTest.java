a77ae0ba44d575beb9fdb4190503ac734fe8f5b6
Ihor Hryshkov <igor.grishkov.olegovich@gmail.com>
2020-08-24 15:10:37 +0300

    PVC-148 - Start add unit tests for callback controller.

--- src/test/java/unit/com/example/ems/network/controllers/CallbackControllerTest.java
+++ src/test/java/unit/com/example/ems/network/controllers/CallbackControllerTest.java
@@ -1,10 +1,16 @@
 package unit.com.example.ems.network.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.example.ems.services.CallbackService;
+import com.example.ems.utils.network.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CallbackControllerTest {
+  @Mock private CallbackService callbackService;
+  @Mock private Response<Object> response;
 
   @Test
   void add() {}
