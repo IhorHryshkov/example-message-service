fb01ebde4c7dc1100f53fa084615ea4151ad0a09
Ihor Hryshkov <igor.grishkov.olegovich@gmail.com>
2020-08-26 05:53:51 +0300

    PVC-148 - Add unit tests for user counter component.

--- src/main/java/com/example/ems/dto/database/pg/Counters.java
+++ src/main/java/com/example/ems/dto/database/pg/Counters.java
@@ -10,7 +10,13 @@ import com.example.ems.dto.database.pg.ids.CountersIds;
 import com.example.ems.utils.converters.pg.LongTimestampConverter;
 import java.io.Serializable;
 import java.math.BigInteger;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,14 +29,16 @@ public class Counters implements Serializable {
   //	@JsonIgnore
   @EmbeddedId private CountersIds keys;
 
-  //	@MapsId("userId")
-  //	@ManyToOne
-  //	@JoinColumn(name = "user_id", referencedColumnName = "id")
-  //	private Users user;
-  //	@MapsId("typeId")
-  //	@ManyToOne
-  //	@JoinColumn(name = "type_id", referencedColumnName = "id")
-  //	private Types type;
+  @MapsId("userId")
+  @ManyToOne
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private Users user;
+
+  @MapsId("typeId")
+  @ManyToOne
+  @JoinColumn(name = "type_id", referencedColumnName = "id")
+  private Types type;
+
   @Column(name = "counts", precision = 18, scale = 0, nullable = false)
   private BigInteger counts;
 
--- src/test/java/unit/com/example/ems/services/components/UserCounterComponentTest.java
+++ src/test/java/unit/com/example/ems/services/components/UserCounterComponentTest.java
@@ -1,24 +1,36 @@
 package unit.com.example.ems.services.components;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import com.example.ems.database.dao.pg.CountersDAO;
 import com.example.ems.database.dao.pg.UsersDAO;
 import com.example.ems.database.dao.redis.StateDAO;
+import com.example.ems.dto.database.pg.Counters;
 import com.example.ems.dto.database.pg.Status;
 import com.example.ems.dto.database.pg.Types;
 import com.example.ems.dto.database.pg.Users;
+import com.example.ems.dto.database.pg.ids.CountersIds;
+import com.example.ems.network.controllers.exceptions.status.UserIDNotFoundException;
+import com.example.ems.services.components.UserCounterComponent;
+import java.math.BigInteger;
+import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class UserCounterComponentTest {
-  private CountersDAO countersDAO;
-  private UsersDAO usersDAO;
-  private StateDAO stateDAO;
+  @Mock private CountersDAO countersDAO;
+  @Mock private UsersDAO usersDAO;
+  @Mock private StateDAO stateDAO;
+
+  @InjectMocks private UserCounterComponent userCounterComponent;
 
   @Test
   void incCounter() {
@@ -30,11 +42,63 @@ class UserCounterComponentTest {
     Users user = new Users();
     user.setId(UUID.fromString("99d4160b-d8e4-4425-a856-b4f2285f9ad5"));
     user.setStatus(new Status("Online"));
+    CountersIds countersIds = new CountersIds(user.getId(), type.getId());
+    Counters counterFind = new Counters(countersIds, BigInteger.valueOf(10));
+    Counters counterNewExpect = new Counters(countersIds, BigInteger.valueOf(1));
+    Counters counterFindAndAddExpect = new Counters(countersIds, BigInteger.valueOf(15));
 
     when(stateDAO.exist(eq(keyExpect), eq(hash))).thenReturn(true, false);
-    when(stateDAO.add(eq(keyExpect), eq(hash), eq(""))).thenReturn("object", null);
+    when(stateDAO.add(eq(keyExpect), eq(hash), eq(""))).thenReturn("object", (Object) null);
+    when(countersDAO.findById(eq(countersIds)))
+        .thenReturn(Optional.empty())
+        .thenReturn(Optional.of(counterFind));
+    when(countersDAO.save(eq(counterNewExpect))).thenReturn(counterNewExpect);
+    when(countersDAO.save(eq(counterFindAndAddExpect))).thenReturn(counterNewExpect);
+
+    // State by key exist in cache
+    userCounterComponent.incCounter(null, user, null, key, hash);
+    // State by key not exist in cache but add new cache data found this data
+    userCounterComponent.incCounter(null, user, null, key, hash);
+    // State by key not exist in cache but add new cache data not found this data and type is null
+    userCounterComponent.incCounter(null, user, null, key, hash);
+    // State by key not exist in cache but add new cache data not found this data and counter in DB
+    // not found and "count" field is null
+    userCounterComponent.incCounter(type, user, null, key, hash);
+    // State by key not exist in cache but add new cache data not found this data and counter in DB
+    // found
+    userCounterComponent.incCounter(type, user, 5L, key, hash);
   }
 
   @Test
-  void getUserOrNotFound() {}
+  void getUserOrNotFound() {
+    String hash = "testHash";
+    String hashUserId = "userIdHash";
+    String key = "testKey::%s";
+    String keyExpect = "testKey::IN_PROGRESS";
+    UUID userId = UUID.fromString("99d4160b-d8e4-4425-a856-b4f2285f9ad5");
+    Users userFind = new Users();
+    userFind.setId(userId);
+    userFind.setStatus(new Status("Online"));
+    when(usersDAO.findById(eq(userId)))
+        .thenReturn(Optional.empty())
+        .thenReturn(Optional.empty())
+        .thenReturn(Optional.of(userFind));
+    when(stateDAO.del(eq(keyExpect), eq(hash)))
+        .thenThrow(new RuntimeException("Test"))
+        .thenReturn(true);
+    assertThat(
+            catchThrowable(
+                () -> userCounterComponent.getUserOrNotFound(userId, key, hash, hashUserId)))
+        .as("Delete have internal exception")
+        .isInstanceOf(RuntimeException.class)
+        .hasMessageContaining("Test");
+    assertThat(
+            catchThrowable(
+                () -> userCounterComponent.getUserOrNotFound(userId, key, hash, hashUserId)))
+        .as("User ID not found exception")
+        .isInstanceOf(UserIDNotFoundException.class);
+    assertThat(userCounterComponent.getUserOrNotFound(userId, key, hash, hashUserId))
+        .as("User ID found")
+        .isEqualTo(userFind);
+  }
 }
