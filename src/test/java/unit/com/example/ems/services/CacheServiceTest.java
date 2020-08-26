3b9a7ad62b1bec31e0daac0293315d89643f4bf5
Ihor Hryshkov <igor.grishkov.olegovich@gmail.com>
2020-08-26 20:15:06 +0300

    PVC-148 - Start add unit tests for cache service.

--- src/test/java/unit/com/example/ems/services/CacheServiceTest.java
+++ src/test/java/unit/com/example/ems/services/CacheServiceTest.java
@@ -1,20 +1,74 @@
 package unit.com.example.ems.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
+import com.example.ems.database.dao.redis.CacheDAO;
+import com.example.ems.network.controllers.exceptions.global.ResponseIfNoneMatchException;
+import com.example.ems.services.CacheService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CacheServiceTest {
+  @Mock private CacheDAO cacheDAO;
+
+  @InjectMocks private CacheService cacheService;
 
   @Test
-  void existOrIfNoneMatch() {}
+  void existOrIfNoneMatch() {
+    String key = "testKey";
+    when(cacheDAO.exist(eq(key))).thenReturn(true, false);
+    assertThat(catchThrowable(() -> cacheService.existOrIfNoneMatch(key)))
+        .as("Key not found exception")
+        .isInstanceOf(ResponseIfNoneMatchException.class);
+    cacheService.existOrIfNoneMatch(key);
+  }
 
   @Test
-  void setKeyForCheckWithTtlDivider() {}
+  void setKeyForCheckWithTtlDivider() {
+    String key = "testKey";
+    Integer divider = 2;
+    doThrow(new RuntimeException("Test")).when(cacheDAO).setTtl(eq(key), eq(""), eq(divider));
+    assertThat(catchThrowable(() -> cacheService.setKeyForCheckWithTtlDivider(key, 2)))
+        .as("Some exception")
+        .isInstanceOf(RuntimeException.class)
+        .hasMessageContaining("Test");
+
+    doNothing().when(cacheDAO).setTtl(eq(key), eq(""), eq(divider));
+    cacheService.setKeyForCheckWithTtlDivider(key, 2);
+  }
 
   @Test
-  void hexistOrIfNoneMatch() {}
+  void hexistOrIfNoneMatch() {
+    String key = "testKey";
+    String hash = "testHash";
+    when(cacheDAO.hexist(eq(key), eq(hash))).thenReturn(true, false);
+    assertThat(catchThrowable(() -> cacheService.hexistOrIfNoneMatch(key, hash)))
+        .as("Key not found exception")
+        .isInstanceOf(ResponseIfNoneMatchException.class);
+    cacheService.hexistOrIfNoneMatch(key, hash);
+  }
 
   @Test
-  void hset() {}
+  void hset() {
+    String key = "testKey";
+    String hash = "testHash";
+    String value = "testJSON";
+    doThrow(new RuntimeException("Test")).when(cacheDAO).hset(eq(key), eq(hash), eq(value));
+    assertThat(catchThrowable(() -> cacheService.hset(key, hash, value)))
+        .as("Some exception")
+        .isInstanceOf(RuntimeException.class)
+        .hasMessageContaining("Test");
+
+    doNothing().when(cacheDAO).hset(eq(key), eq(hash), eq(value));
+    cacheService.hset(key, hash, value);
+  }
 }
